ad14c7111e2c4bcfa61a38a0ed64f976538ae574
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2024-09-30 15:54:22 +0700

    1

--- src/components/PostOwnner.tsx
+++ src/components/PostOwnner.tsx
@@ -1,4 +1,32 @@
 "use client";
-export default function PostOwnner({}) {
-  return <div>Your code for PostOwner component ...</div>;
+import { PostProps } from "@/libs/types";
+export default function PostOwnner({username, likes, userImagePath}: PostProps) {
+  return <div>
+    <div className="vstack gap-3">
+
+        <div className="d-flex align-items-center gap-3">
+          <img
+
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5">{username}</span>
+
+          </div>
+
+          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+
+          <div className="d-flex align-items-center gap-1">
+
+            <img src="/like.svg" width={20}></img>
+
+            <span className="text-muted">{likes} คน</span>
+            
+          </div>
+          <hr className="m-0 border" />
+        </div>
+  </div>;
 }
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,32 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
+interface CommentProps {
+    userImagePath: string;
+    username: string;
+    commentText: string;
+    likeNum: number;
+    replies: ReplyProps[];
 
-interface ReplyProps {}
+}
+export type { CommentProps };
+
+
 export type { ReplyProps };
+
+interface PostProps {
+    userImagePath: string;
+    username: string;
+    likes: number;
+}
+export type { PostProps };
+
+
+
+interface ReplyProps {
+    userImagePath: string;
+    username: string;
+    replyText: string;
+    likeNum: number;
+}
+
+
